a6eed4878f776d1a720e1b5b76b19b82f7d42f9c
robert <robertishimwe0@gmail.com>
2022-09-28 13:21:43 +0200

    added populate functionality on health practitioner

--- src/routes/appointment.route.js
+++ src/routes/appointment.route.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import AppointmentController from '../controller/appointmentBooking'
 import appointmentValidation from '../validations/appointment.validation'
+import AdimnAppointmentController from '../controller/adminAppointment';
 import PatientAppointmentController from '../controller/patientsAppointment'
 import PractitionerAppointmentController from '../controller/practitionersAppointment'
+import HealthCenterAdimnAppointmentController from '../controller/healthAdminAppointment';
 import authorize from '../middleware/authorize'
 import verify from '../middleware/verify';
 
@@ -12,8 +14,11 @@ const { appointmentBookingValidation} = appointmentValidation
 const router = Router();
 
 router.post('/booking',verify,isPatient,appointmentBookingValidation,AppointmentController.patientsBookingAppointment)
+router.get('/admin/my-appointments',verify,isAdmin,AdimnAppointmentController.ViewPractitionersAppointments)
 router.get('/patient/my-appointments',verify,isPatient,PatientAppointmentController.ViewPatientAppointments)
 router.get('/practioner/my-appointments',verify,isHealthPractional,PractitionerAppointmentController.ViewPractitionersAppointments)
+router.get('/health-center/my-appointments',verify,isHospitalAdmin,HealthCenterAdimnAppointmentController.ViewPractitionersAppointments)
+
 
 // router.post('/register/healthPractional',verify,isHospitalAdmin, healthPractionalregistrationValidation, AuthController.healthPractictionerRegistration)
 
