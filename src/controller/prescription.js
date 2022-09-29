8ef1d6373f4a468c47a9967f3baa315a277bd08a
robert <robertishimwe0@gmail.com>
2022-09-29 22:47:56 +0200

    fin

--- src/controller/prescription.js
+++ src/controller/prescription.js
@@ -1,5 +1,7 @@
 import Prescription from '../models/prescription';
 import prescriptionService from '../services/prescription.service';
+import PrescriptionTemplate from '../helpers/prescriptionTemplate'
+import emailHelper from '../helpers/email';
 import userService from '../services/user.service';
 
 const { addNewPrescription } = prescriptionService;
@@ -24,6 +26,7 @@ class PrescriptionController {
 
 		try {
 			const data = await addNewPrescription(wholePrescription);
+			await emailHelper(data.patient.email,`Prescription - ${data.hospital.hospitalName}`, PrescriptionTemplate(`${data.patient.firstName} ${data.patient.lastName}`,data.patient.email,`${data.healthPractional.firstName} ${data.healthPractional.lastName}`,data.hospital.hospitalName,data?.hospital.email,data?.prescribedMedications[0]?.medicationName,data?.prescribedMedications[0]?.purpose,data?.prescribedMedications[0]?.Dosage,data?.prescribedMedications[0]?.frequency,data?.prescribedMedications[1]?.medicationName,data?.prescribedMedications[1]?.purpose,data?.prescribedMedications[1]?.Dosage,data?.prescribedMedications[1]?.frequency,data?.prescribedMedications[2]?.medicationName,data?.prescribedMedications[2]?.purpose,data?.prescribedMedications[2]?.Dosage,data?.prescribedMedications[2]?.frequency))
 			return res.status(200).send({ data: data });
 		} catch (error) {
 			return res.status(500).send({ error: error.message });
--- src/services/prescription.service.js
+++ src/services/prescription.service.js
@@ -4,7 +4,7 @@ class prescriptionService {
     static addNewPrescription = async (data) =>{
 
         const prescription = new Prescription(data);
-		const newPrescription = await prescription.save();
+		const newPrescription = await (await (await (await prescription.save()).populate("patient")).populate("healthPractional")).populate("hospital");
 
         if(newPrescription) return newPrescription
 		
